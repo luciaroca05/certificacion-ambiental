8275f1141e45210a78efb6b472bf803a8fe3ae22
DONA <macintosh@DONITA.local>
2022-06-11 15:33:09 -0500

    Slider agregado

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
+"""
+git add . && git commit -m "" && git push
+"""
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+from datetime import datetime
 st.title('Test')
 fecha_inicio = st.slider("Ver informacion registrada en:",
     value = datetime(2020,1,1,9,30),
