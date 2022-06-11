6d85b5ba9fd182707258c1fd02fb7c54af0af202
DONA <macintosh@DONITA.local>
2022-06-11 15:41:12 -0500

    Widgets agregados

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,14 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 st.title('Test')
+
 fecha_inicio = st.slider("Ver informacion registrada en:",
     value = datetime(2020,1,1,9,30),
     format = "DD/MM/YY - hh:mm")
 st.write("Fecha seleccionada:", fecha_inicio)
+
+
+opcion_dataset = st.selectbox(
+     '¿Qué dataset deseas visualizar?',
+     ('Aprobado', 'Desaprobado', 'Pendiente'))
+st.write('Seleccionó:', opcion_dataset)
