38acfc2a3c9c3229b3ccb61643fb0cca58a4b60b
DONA <macintosh@DONITA.local>
2022-06-12 18:22:15 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,12 @@ https://github.com/luciaroca05/proyecto-final-certificacion-ambiental/
 https://share.streamlit.io/luciaroca05/proyecto-final-certificacion-ambiental/main
 cd Desktop
 cd proy (TAB)
+streamlit run streamlit_app.py
 git pull && git add . && git commit -m "." && git push
+
 """
 
-$ pip install plotly
+#----------------------------------INSTALACIÓN----E----IMPORTACIÓN--------------------------------------
 
 # Importación de bibliotecas
 import pandas as pd
@@ -15,6 +17,7 @@ import numpy as np
 import streamlit as st
 from datetime import datetime
 import matplotlib.pyplot as plt
+from PIL import Image
 
 # Librerias y modulos de ML, DA
 import sklearn as sk
@@ -27,12 +30,19 @@ from sklearn.neural_network import MLPClassifier
 from io import BytesIO
 from pyxlsb import open_workbook as open_xlsb
 
-# Carga de datos
+#------------------------------------------------MACHINE----LEARNING--------------------------------------------
+
+# Obtención de datos
+
+## Carga de datos
 df_aprobado = pd.read_json('datasets/Reporte_Proyecto_APROBADO.json')
 df_desaprobado = pd.read_json('datasets/Reporte_Proyecto_DESAPROBADO.json')
 df_evaluacion = pd.read_json('datasets/Reporte_Proyecto_EN EVALUACION.json')
 
+# Limpiar, Preparar y Manipular Data
+
 # Transformacion del formato de fechas
+
 df_aprobado['FECHA_INICIO'] = df_aprobado['FECHA_INICIO'].astype('datetime64[ns]')
 df_desaprobado['FECHA_INICIO'] = df_desaprobado['FECHA_INICIO'].astype('datetime64[ns]')
 df_evaluacion['FECHA_INICIO'] = df_evaluacion['FECHA_INICIO'].astype('datetime64[ns]')
@@ -49,10 +59,12 @@ df_evaluacion['AÑO'] = df_evaluacion['FECHA_INICIO'].apply(lambda time: time.ye
 df_evaluacion['MES'] = df_evaluacion['FECHA_INICIO'].apply(lambda time: time.month)
 df_evaluacion['DIA'] = df_evaluacion['FECHA_INICIO'].apply(lambda time: time.day)
 
-# Swap de latitud y longitud mal ingresadas (No hay proyecto en el antártida, por debajo de latitud -74)
+# Limpieza de datos
+
+## Swap de latitud y longitud mal ingresadas (No hay proyecto en el antártida, por debajo de latitud -74)
 df_aprobado['LATITUD'],df_aprobado['LONGITUD']=np.where(df_aprobado['LATITUD']<-74,(df_aprobado['LONGITUD'],df_aprobado['LATITUD']),(df_aprobado['LATITUD'],df_aprobado['LONGITUD']))
 
-# Reemplazo de valoes duplicados en la columna ACTIVIDAD (Residuos Sólidos y Transporte)
+## Reemplazo de valores duplicados en la columna ACTIVIDAD (Residuos Sólidos y Transporte)
 df_aprobado['ACTIVIDAD'] = df_aprobado['ACTIVIDAD'].replace('Residuos Solidos','Residuos Sólidos')
 df_aprobado['ACTIVIDAD'] = df_aprobado['ACTIVIDAD'].replace('Transportes','Transporte')
 
@@ -62,141 +74,120 @@ df_desaprobado['ACTIVIDAD'] = df_desaprobado['ACTIVIDAD'].replace('Transportes',
 df_evaluacion['ACTIVIDAD'] = df_evaluacion['ACTIVIDAD'].replace('Residuos Solidos','Residuos Sólidos')
 df_evaluacion['ACTIVIDAD'] = df_evaluacion['ACTIVIDAD'].replace('Transportes','Transporte')
 
-# Unificación de datas para visualización (aprobados y desaprobados)
+## Unificación de datas para visualización (aprobados y desaprobados)
 df_unificado =  pd.concat([df_aprobado, df_desaprobado])
 
 
 # Preparacion de datos
 
-# Selección de registros o entradas
-"""
-953
-92
-----
-Correcion de datos (OK)
-953
-92
-
-Aprobados: 953 = 800+153 (OK)
-Desaprobados: 92 = 80+12 (OK)
-
-Dataset con el 80% aprox. de la data original (OK)
-Aprobados: 800 (obtenidos aleatoriamente) (OK)
-Desaprobados: 80 (obtenidos aleatoriamente) (OK)
-Desaprobados: 80x10 = 800 (obtenidos duplicando datos) (OK)
---------------------------------------------------
-800+800 = 1600 datos (50%A y 50%D) Data balanceada
-
-Modelo entrenado con 1600 -> modelo_clasificador
-
-Para demostrar que es bueno, vas a seleccionar los 165 (153+12) datos
-restantes con eso le demuestras a tu profe que esta bueno el modelo
-
-Aplicas el modelo sobre la data que aun esta
-pendiente de clasificacion (13 registros)
-"""
-# Dataframe aprobado se divide en 2: part1 (800) y parte2 (153)
-df_aprobado_parte1  = df_aprobado.sample(n = 800, random_state=1)
+## Dataframe aprobado se divide en 2: part1 (800) y parte2 (153)
+df_aprobado_parte1  = df_aprobado.sample(n = 800, random_state=1) # Esto para entrenamiento del modelo
 df_aprobado_parte2 = df_aprobado.drop(df_aprobado_parte1.index)
 
-# Reseteo de los indices de los datsets generados
+## Reseteo de los indices de los datsets generados
+
 df_aprobado_parte1.reset_index()
 df_aprobado_parte2.reset_index()
 
-#df_aprobado_parte1.info()
-#df_aprobado_parte2.info()
+## Dataframe desaprobado se divide en 2: part1 (80) y parte2 (12)
 
-# Dataframe desaprobado se divide en 2: part1 (80) y parte2 (12)
-df_desaprobado_parte1  = df_desaprobado.sample(n = 80, random_state=1) # Esto para va train_test
+df_desaprobado_parte1  = df_desaprobado.sample(n = 80, random_state=1)
 df_desaprobado_parte2 = df_desaprobado.drop(df_desaprobado_parte1.index)
 
-# Reseteo de los indices de los datsets generados
+## Reseteo de los indices de los datsets generados
+
 df_desaprobado_parte1.reset_index()
 df_desaprobado_parte2.reset_index()
 
-#df_desaprobado_parte1.info()
-#df_desaprobado_parte2.info()
+## Repetir 10 veces los 80 datos de la parte 1 del dataset desprobado = 10x80 = 800
 
-# Repetir 10 veces los 80 datos de la parte 1 del dataset desprobado = 10x80 = 800
-df_desaprobado_parte1 =  pd.concat([df_desaprobado_parte1]*10)
+df_desaprobado_parte1 =  pd.concat([df_desaprobado_parte1]*10) # Esto para entrenamiento del modelo
 
+## Creacion del dataframe para entrenamiento (train) y test del modelo
 
-
-
-
-# Creacion del dataframe para train y test del modelo
 df_train =  pd.concat([df_aprobado_parte1, df_desaprobado_parte1])
 df_test = pd.concat([df_aprobado_parte2, df_desaprobado_parte2])
 
 df_train.reset_index()
 df_test.reset_index()
 
-df_train.info()
+## Separacion de columnas X y columna y para a partir de los dataframe de train y test
 
-
-
-# Separacion de columnas X y columna y para a partir de los dataframe de train y tet
 y_train = df_train.iloc[:,8]
-X_train = df_train.iloc[:,[10,11,14,15]]
+X_train = df_train.iloc[:,[10,11,14,15]]# elección de columnas para predicción (Longitud, latitud, año, mes)
 
 y_test = df_test.iloc[:,8]
-X_test = df_test.iloc[:,[10,11,14,15]]
+X_test = df_test.iloc[:,[10,11,14,15]]# elección de columnas para predicción (Longitud, latitud, año, mes)
 
 
-
-
-
-
+# Entrenamiento y test de los modelos para la predicción
 
 LR = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X_train, y_train)
-#LR.predict(X_test)
-print(round(LR.score(X_test,y_test), 4))
-
 SVM = svm.SVC(decision_function_shape="ovo").fit(X_train, y_train)
-#SVM.predict(X_test)
-print(round(SVM.score(X_test, y_test), 4))
-
 RF = RandomForestClassifier(n_estimators=1000, max_depth=10, random_state=0).fit(X_train, y_train)
-#RF.predict(X_test)
-print(round(RF.score(X_test, y_test), 4))
-
 NN = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(10, 3), random_state=1).fit(X_train, y_train)
-print(round(NN.score(X_test, y_test), 4))
 
-"""
-print("NN")
-for i in range(7,11):
-  for j in range(2,6):
-    NN = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(i, j), random_state=1, max_iter=300).fit(X_train, y_train)
-    acc = round(NN.score(X_test, y_test), 4)
-    if acc > 0.8:
-      print(i,j,acc)
-"""  
+#-----------------------------------------CÓDIGOS---DEL---APP---------------------------------------------------
 
-
-
-
-
-
-
-#----------------------------------------
 #App
-st.title('Certificación Ambiental - Proyecto Final')
+
+#Título
+st.markdown("<h1 style='text-align: center; color: black;'>Certificación Ambiental - Proyecto Final</h1>", unsafe_allow_html=True)
+st.markdown("##") # Linea en blanco
+
+#Bienvenida
+st.markdown("<h1 style='text-align: center; color: black;'>Bienvenidx a nuestra APP 😄!</h1>", unsafe_allow_html=True)
 st.markdown("---") # Linea divisoria
 st.markdown("##") # Linea en blanco
-st.write('Bienvenidx al **app**')
 
+#Introducción al grupo
+st.header('Nosotras')
+st.markdown("##") # Linea en blanco
+st.caption('Grupo de estudiantes del 5to ciclo de la carrera de Ingeniería Ambiental de UPCH del curso de Programación Avanzada')
 
-#Proyectos con fecha
-#st.write('Proyectos por fecha')
+#Integrantes con foto
+image = Image.open('integrantes.jpg')
+st.image(image, caption='Dona Nicole Chancan Aviles, María Fernanda Cisneros Moron, Sharon Nicolle Dextre Cartolin, Daniella Mercedes Palacios Li, Lucía Fernanda Roca Cuadros')
+st.markdown("##") # Linea en blanco
 
-#fecha_inicio = st.slider("Ver informacion registrada en:",
-#    value = datetime(2020,1,1,9,30),
-#    format = "DD/MM/YY - hh:mm")
-#st.write("Fecha seleccionada:", fecha_inicio)
+st.markdown("---") # Linea divisoria
+
+#Sobre el APP
+st.header('Sobre el APP')
+st.markdown("##") # Linea en blanco
+'''
+En esta APP se va a relizar el análisis, visualización y clasificación, utilizando datos sobre certificación
+ambiental del Servicio Nacional de Certificación Ambiental para las Inversiones Sostenibles – SENACE.
+
+Fuente: https://www.datosabiertos.gob.pe/dataset/certificaci%C3%B3n-ambiental
+
+Los datos están clasificados en las siguientes categorías,
+'''
+st.write('ID: ') 
+st.write('Titular: ')
+st.write('RUC: Registro Único de Contribuyentes')
+st.write('Título Proyecto: ')
+st.write('Unidad Proyecto: ')
+st.write('Tipo: ')
+st.write('Actividad: ')
+st.write('Fecha de inicio: ')
+st.write('Estado: ')
+st.write('Descripción: ')
+st.write('Longitud y Latitud: Ubicación ')
+st.write('Resolución: ')
+st.write('Label: ')
+
+st.markdown("---") # Linea divisoria
+
+#-------------------------------------
 
 # Seleccion del dataset
-st.write('Seleccionar proyecto por estado')
+st.header('Análisis exploratorio')
+st.markdown("---")
+st.subheader('Seleccione proyecto por estado (aprobados, desaprobados o en evaluación)')
+
+st.markdown("##") # Linea en blanco
+
 opcion_dataset = st.selectbox(
     '¿Qué dataset deseas visualizar?',
     ('Proyectos aprobados',
@@ -215,61 +206,84 @@ elif opcion_dataset == 'Proyectos en evaluacion':
     df_visualizacion = df_evaluacion
     estado = 'en evaluación'
 
-st.write('Seleccionó visualizar',len(df_visualizacion.index),'proyectos',estado,'en total:')
-
-# Agregar  un widget quemuestre metadatos (cols, rows, no nulls, etc)
-
-
-
 # Cruces de datos plot(x,y)
+
+## Pie de frecuencia de actividad
+t1 = '• Frecuencia de los proyectos '+estado+' según la clasificación ACTIVIDAD'
+st.subheader(t1)
+st.markdown("##")
 df_actividad_freq = pd.DataFrame(df_visualizacion["ACTIVIDAD"].value_counts())
 labels = df_actividad_freq.index.tolist()
 sizes = df_actividad_freq["ACTIVIDAD"].tolist()
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
 startangle=0)
-plt.title('Distribucion de datos segun ACTIVIDAD')
+#plt.title('Distribucion de datos segun ACTIVIDAD')
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 st.pyplot(fig1)
+st.write('Figura 1. Gráfica pie de los proyectos con la frecuencia según el ACTIVIDAD de proyecto.')
 
+## Pie de frecuencia de tipo
+t2 = '• Frecuencia de los proyectos '+estado+' según la clasificación TIPO' 
+st.subheader(t2)
+st.markdown("##")
 df_tipo_freq = pd.DataFrame(df_visualizacion["TIPO"].value_counts())
 labels = df_tipo_freq.index.tolist()
 sizes = df_tipo_freq["TIPO"].tolist()
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
 startangle=0, textprops={'fontsize': 10})
-plt.title('Distribucion de datos segun TIPO')
+#plt.title('Distribucion de datos segun TIPO')
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 st.pyplot(fig1)
+st.write('Figura 2. Gráfica pie de los proyectos con la frecuencia según el TIPO de proyecto.')
 
-# Agrega titulo con write
+## Cantidad de proyectos por año
+t3 = '• Cantidad de proyectos '+estado+' presentados en diferentes años'
+st.subheader(t3)
+st.markdown("##")
 df_anho_freq = pd.DataFrame(df_visualizacion["AÑO"].value_counts())
 st.bar_chart(df_anho_freq)
+st.write('Figura 3. Gráfica de barras de los proyectos clasificados por año de inicio.')
 
-#print(df_actividad_freq.head())
-#st.dataframe(df_actividad_freq)
-#st.bar_chart(df_anho_freq)
-
-# Visualizacion de datos en el mapa
-st.write('Ubicación de los proyectos',estado)
+## Visualizacion de datos en el mapa
+t4 = '• Proyectos '+estado+' localizados en un mapa interactivo mundial'
+st.subheader(t4)
+st.markdown("##") # Linea en blanco
 df_visualizacion = df_visualizacion.rename(columns={'LATITUD':'lat', 'LONGITUD':'lon'})
 st.map(df_visualizacion[['lat','lon']])
+st.write('Figura 4. Ubicación de los proyectos',estado,'.')
 
-# Tabla de datos (ver si se puede agregar widget de busqueda)
-st.write('Tabla de datos',estado,'en formato DataFrame')
+## Tabla de datos (ver si se puede agregar widget de busqueda)
+t5 = '• Se pueden visualizar '+str(len(df_visualizacion.index))+' proyectos '+estado+' :'
+st.subheader(t5)
+st.markdown("##") # Linea en blanco
 st.dataframe(df_visualizacion)
+st.markdown("##") # Linea en blanco
+st.write('Tabla 1. Tabla de datos',estado,'en formato DataFrame.')
 
-# Analisis predictivo
-# Poner un texto que indique que haciendo uso de los dtasets de aprobados y desprobados
-# se entrenaron varios modelos de clasificacion, utilizando las columnas
-# lat y long (numericos), versus ESTADO (categorico)
-# Se permitira al usuario seleccionar el tipo de modelo para mostrar el accuracy
-# y en base a ellos se calificara a los datasets que aun estan evaluacion
-# ...
+st.markdown("---") # Linea divisoria
+#-----------------------------------------2DO----ANÁLISIS-----------------------------------------
 
+st.header('Análisis predictivo')
+st.subheader('Aplicación de los 5 pasos fundamentales para Machine Learning (Fig. 5.)')
+st.markdown("---")
+
+'''
+Haciendo uso de los datasets de aprobados y desprobados, se entrenaron modelos de clasificación. Se utilizaron
+las columnas de longitud, latitud, año y mes como X para predecir Y, que vendría a ser el Estado (categórico).
+
+A continuación, usted podrá elegir el tipo de modelo (LR, SVM, RF, NN) que desee usar para obtener el archivo
+donde se clasifica los proyectos que se encuentran en estado de evaluación como Aprobado o Desaprobado.
+'''
+
+
+
+st.markdown("##") # Linea en blanco
 st.write('Seleccionar tipo de modelo que se desea entrenar')
+st.markdown("##") # Linea en blanco
 opcion_modelo = st.selectbox(
-     '¿Qué modelo deseas utilizar pa el entranamiento?',
+     '¿Qué modelo deseas utilizar para el entrenamiento y posterior clasificación?',
      ('Logistic Regression','Support Vector Machine','Random Forest','Neural Network'))
 modelo = None
 if opcion_modelo == 'Logistic Regression':
@@ -280,11 +294,14 @@ elif opcion_modelo == 'Random Forest':
     modelo = RF
 elif opcion_modelo == 'Neural Network':
     modelo = NN
+st.markdown("##") # Linea en blanco
 
-st.write('El score del modelo',opcion_modelo,'es:',round(modelo.score(X_test, y_test), 4))
+text2 = '• El score del modelo '+opcion_modelo+' es: '+str(round(modelo.score(X_test, y_test), 4))
+st.subheader(text2)
 
-st.write("Se clasificaron los datos quetenian estado PENDIENTE utiliznado el modelo",opcion_modelo)
-st.write("Pulsar en el boton para descagr la informacion ya clasificada")
+st.markdown("##")
+st.subheader("Se clasificaron los datos que tenían estado EN EVALUACIÓN utilizando el modelo",opcion_modelo)
+st.write("Pulsar en el botón para descargar la información ya clasificada")
 
 X_gen = df_evaluacion.iloc[:,[10,11,14,15]]
 y_gen = pd.DataFrame(modelo.predict(X_test))
@@ -298,13 +315,15 @@ def to_excel(df):
     workbook = writer.book
     worksheet = writer.sheets['Sheet1']
     format1 = workbook.add_format({'num_format': '0.00'}) 
-    worksheet.set_column('A:A', None, format1)  
+    worksheet.set_column('A:A', None, format1)
     writer.save()
     processed_data = output.getvalue()
     return processed_data
 
 df_xlsx = to_excel(df_evaluacion_gen)
-st.download_button(label='📥 Download Current Result',
+
+st.markdown("##")
+st.download_button(label='📥 Descargar aquí',
                                 data=df_xlsx ,
                                 file_name= 'df-evaluacion-clasificada-'+opcion_modelo.replace(' ','-').lower()+'.xlsx')
 
